74801aaac56ea04361f51a6117757ed78a545fe5
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-05-17 10:06:31 -0500

    Add files via upload

--- Port/src/themes/Theme3.js
+++ Port/src/themes/Theme3.js
@@ -1,9 +1,64 @@
-
+import { Container, Row, Col, Button } from "reactstrap"
+import '../themes/Theme3.css';
 
 function Theme3(){
-  return(
-    <div>
-      <h1>theme3</h1>
+  return (
+    <div className="Theme3">
+      <h1>Accordion style</h1>
+      <Button className="btn3">
+        Sample Button
+      </Button>
+      <Container className="containerT31">
+        <Row>
+          <Col>
+          <h4>column 1</h4>
+          <p className="p3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Button className="btn3">Button 1</Button>
+          </Col>
+          <Col>
+          <h4>column 2</h4>
+          <p className="p3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Button className="btn3">Button 2</Button>
+          </Col>
+          <Col>
+          <h4>column 3</h4>
+          <p className="p3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Button className="btn3">Button 3</Button>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="containerT32">
+      <Row>
+        <Col>
+        <p className="p3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Button className="btn3">Button 1</Button>
+        </Col>
+        <Col>
+        <p className="p3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Button className="btn3">Button 2</Button>
+        </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
